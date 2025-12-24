4f4c850edac100fc8b6435b2729b72e8b912a56c
Sai Charan <charanasapu16@gmail.com>
2025-12-23 19:37:50 -0600

    fix build script and prisma export

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -8,9 +8,7 @@ declare global {
   var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
-
-export default prisma;
+export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
 
